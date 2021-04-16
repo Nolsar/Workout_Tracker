814794e1b33c6293d7dfb90f2c8420a07ba52498
Sara Nolan <saraa.ann28@gmail.com>
2021-04-16 19:45:27 -0400

    added routes

--- server.js
+++ server.js
@@ -4,11 +4,9 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
-
-// const User = require("./userModel.js");
-
 const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 app.use(logger("dev"));
 
@@ -17,32 +15,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/custommethoddb", { useNewUrlParser: true });
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-// Routes
-
-// // Route to post our form submission to mongoDB via mongoose
-// app.post("/submit", ({body}, res) => {
-//   // const {body} = req;
-//   // Create a new user using req.body
-//   const user = new User(body);
-
-//   // Update this route to run the `setFullName` and `lastUpdatedDate` methods before creating a new User
-//   // You must create these methods in the model.
-
-//   user.setFullName();
-//   user.lastUpdatedDate();
-
-//   User.create(user)
-//     .then(dbUser => {
-//       // If saved successfully, send the the new User document to the client
-//       res.json(dbUser);
-//     })
-//     .catch(err => {
-//       // If an error occurs, send the error to the client
-//       res.json(err);
-//     });
-// });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 // Start the server
 app.listen(PORT, () => {
