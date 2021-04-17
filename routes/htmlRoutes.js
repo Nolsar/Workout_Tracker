42ad194a30f6b24006b58a22d402b06f7ec12656
Sara Nolan <saraa.ann28@gmail.com>
2021-04-17 09:00:40 -0400

    fixed error on GET /exercises

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,16 +1,17 @@
 var path = require("path");
 
 module.exports = function(app){ 
-  app.get("/exercise",function (req,res){   
-      res.sendFile(path.join(__dirname,"../Develop/public/exercise.js"));
+  app.get("/exercise",function (req,res){ 
+    res.sendFile(path.join(__dirname, '../Develop/public', 'exercise.html'));  
+    //   res.sendFile(path.join(__dirname,"../Develop/public/exercise.js"));
   });
 
   app.get("/",function(req,res){    
-      res.sendFile(path.join(__dirname,"../Develop/public/index.html"));
+      res.sendFile(path.join(__dirname,'../Develop/public', 'index.html'));
   });
 
   app.get("/stats",function(req,res){   
-      res.sendFile(path.join(__dirname,"../Develop/public/stats.html"));
+      res.sendFile(path.join(__dirname,'../Develop/public', 'stats.html'));
   });
 
 }
